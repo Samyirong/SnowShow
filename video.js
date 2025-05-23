61760df3c573a8ce489cdd50fe263a7ba3e3535e
Sam Yirong <sam.yirong@gmail.com>
2025-05-23 11:06:27 +0800

    change folder

---
+++ video.js
@@ -0,0 +1,28 @@
+$(function () { 
+    $("#myVideo").attr("src", "video/video.mp4"); 
+    $("#myVideo1").attr("src", "video/video1.mp4"); 
+    $("#playBtn").on("click", function () { 
+        if ($("#myVideo")[0].paused) { 
+            $("#myVideo")[0].play(); 
+            $("#playBtn").text("Pause"); 
+        } else { 
+            $("#myVideo")[0].pause(); 
+            $("#playBtn").text("Play"); 
+        } 
+    }); 
+    $("#fullBtn").on("click", function () { 
+        $("#myVideo")[0].webkitEnterFullscreen(); 
+    }); 
+    $("#playBtn1").on("click", function () { 
+        if ($("#myVideo1")[0].paused) { 
+            $("#myVideo1")[0].play(); 
+            $("#playBtn1").text("Pause"); 
+        } else { 
+            $("#myVideo1")[0].pause(); 
+            $("#playBtn1").text("Play"); 
+        } 
+    }); 
+    $("#fullBtn1").on("click", function () { 
+        $("#myVideo1")[0].webkitEnterFullscreen(); 
+    }); 
+});
